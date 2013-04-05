842008a426033f78d27a79e0d76833a40389046c
Ley Missailidis <moisis.langley@missailidis.ca>
2013-04-05 00:47:52 -0300

    bumps version

--- manifests/init.pp
+++ manifests/init.pp
@@ -5,7 +5,7 @@
 #   include textwrangler
 class textwrangler {
   package { 'TextWrangler':
-    source   => 'http://ash.barebones.com/TextWrangler_4.5.dmg',
+    source   => 'http://ash.barebones.com/TextWrangler_4.5.1.dmg',
     provider => 'appdmg'
   }
 }
--- spec/classes/textwrangler_spec.rb
+++ spec/classes/textwrangler_spec.rb
@@ -3,7 +3,7 @@ require 'spec_helper'
 describe 'textwrangler' do
   it do
     should contain_package('TextWrangler').with({
-      :source   => 'http://ash.barebones.com/TextWrangler_4.5.dmg',
+      :source   => 'http://ash.barebones.com/TextWrangler_4.5.1.dmg',
       :provider => 'appdmg'
     })
   end
