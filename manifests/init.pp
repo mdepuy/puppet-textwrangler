72835b7576b0237091776c2af8e090ac8d251250
Ley Missailidis <moisis.langley@missailidis.ca>
2013-06-05 13:20:54 -0300

    updates version

--- manifests/init.pp
+++ manifests/init.pp
@@ -5,7 +5,7 @@
 #   include textwrangler
 class textwrangler {
   package { 'TextWrangler':
-    source   => 'http://ash.barebones.com/TextWrangler_4.5.1.dmg',
+    source   => 'http://pine.barebones.com/files/TextWrangler_4.5.2.dmg',
     provider => 'appdmg'
   }
 }
--- spec/classes/textwrangler_spec.rb
+++ spec/classes/textwrangler_spec.rb
@@ -3,7 +3,7 @@ require 'spec_helper'
 describe 'textwrangler' do
   it do
     should contain_package('TextWrangler').with({
-      :source   => 'http://ash.barebones.com/TextWrangler_4.5.1.dmg',
+      :source   => 'http://pine.barebones.com/files/TextWrangler_4.5.2.dmg',
       :provider => 'appdmg'
     })
   end
