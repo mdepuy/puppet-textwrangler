9903f6d0bef8f00721e490ecdf62a86939f9b6ec
Ryan Gibbons <rtgibbons23@gmail.com>
2013-08-28 16:15:23 -0600

    Updating test with the new version also.

--- spec/classes/textwrangler_spec.rb
+++ spec/classes/textwrangler_spec.rb
@@ -3,7 +3,7 @@ require 'spec_helper'
 describe 'textwrangler' do
   it do
     should contain_package('TextWrangler').with({
-      :source   => 'http://pine.barebones.com/files/TextWrangler_4.5.2.dmg',
+      :source   => 'http://pine.barebones.com/files/TextWrangler_4.5.3.dmg',
       :provider => 'appdmg'
     })
   end
