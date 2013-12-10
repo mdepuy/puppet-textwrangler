f718f49aad0fa578a24fd32ec68f500a0c31fb19
Trevor Cooper <tcooper@sdsc.edu>
2013-12-10 09:48:52 -0800

    Bumped version to 4.5.4

--- spec/classes/textwrangler_spec.rb
+++ spec/classes/textwrangler_spec.rb
@@ -3,7 +3,7 @@ require 'spec_helper'
 describe 'textwrangler' do
   it do
     should contain_package('TextWrangler').with({
-      :source   => 'http://pine.barebones.com/files/TextWrangler_4.5.3.dmg',
+      :source   => 'http://pine.barebones.com/files/TextWrangler_4.5.4.dmg',
       :provider => 'appdmg'
     })
   end
