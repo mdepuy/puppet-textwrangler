df07580c9e114503048a0cdcb52eb1a3ceaa5524
Clayton Burlison <clburlison@gmail.com>
2014-04-18 07:58:46 -0500

    Update to 4.5.8

--- manifests/init.pp
+++ manifests/init.pp
@@ -5,7 +5,7 @@
 #   include textwrangler
 class textwrangler {
   package { 'TextWrangler':
-    source   => 'http://ash.barebones.com/TextWrangler_4.5.7.dmg',
+    source   => 'http://ash.barebones.com/TextWrangler_4.5.8.dmg',
     provider => 'appdmg'
   }
 }
